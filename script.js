1cc564db617112b7ad4747b161383461339add08
lunna <150831224+taecisne@users.noreply.github.com>
2024-01-11 13:07:29 -0300

    desenvolvimento 3.2

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Capturando os elementos
-    var titulo = document.getElementById('titulo');
-    var listaNaoOrdenada = document.querySelector('ul');
-    var link = document.getElementById('link');
-    var listaOrdenada = document.getElementById('lista-ordenada');
+function imprimirMensagem() {
+    console.log("Conexão feita com sucesso!");
+}
 
-    // Adicionando conteúdo aos elementos
-    titulo.innerText = 'Título do Projeto';
-    link.innerText = 'Visite Proz Educação';
-
-    // Adicionando itens à lista não ordenada
-    listaNaoOrdenada.innerHTML = '<li>Item 1</li><li>Item 2</li><li>Item 3</li>';
-
-    // Adicionando itens à lista ordenada com links
-    listaOrdenada.innerHTML = '<li><a href="https://www.siteone.com">Link 1</a></li>' +
-                              '<li><a href="https://www.sitetwo.com">Link 2</a></li>' +
-                              '<li><a href="https://www.sitethree.com">Link 3</a></li>';
-});
+// Executando a função três vezes
+imprimirMensagem();
+imprimirMensagem();
+imprimirMensagem();
